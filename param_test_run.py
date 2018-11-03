8bf517b9cd6d5aaa74d35ffc6511817e57574abc
AF018 <a.francois962@laposte.net>
2018-11-03 17:53:45 +0100

    A few modifications on the parameters test scripts

--- param_test_run.py
+++ param_test_run.py
@@ -8,12 +8,13 @@ temperature_file = open("temperature_list.csv", "r")
 temperature_values_list = temperature_file.read().splitlines()
 
 for radius_values in radius_values_list:
+    capt_radius, comm_radius = radius_values.split(" ")
     for instance_name in instance_names_list:
         for temperature_values in temperature_values_list:
 
             with open("run_parameters.dat", "w") as parameters_file:                
                 with open("test_parameters_template.dat", "r") as template_parameters_file:
                     for template_line in template_parameters_file:
-                        parameters_file.write(template_line.replace("INSTANCE_NAME", instance_name).replace("CAPT_RADIUS COMM_RADIUS", radius_values).replace("TEMPERATURE_VALUES", temperature_values))
+                        parameters_file.write(template_line.replace("INSTANCE_NAME", instance_name).replace("CAPT_RADIUS", capt_radius).replace("COMM_RADIUS", comm_radius).replace("TEMPERATURE_VALUES", temperature_values))
             os.system("echo " + instance_name + " RADIUS " + radius_values + " TEMPERATURE " + temperature_values)
             os.system("Metaheuristics_solution\\Debug\\Metaheuristics_solution.exe")
--- run.py
+++ run.py
@@ -6,11 +6,12 @@ instance_file = open("instance_list.csv", "r")
 instance_names_list = instance_file.read().splitlines()
 
 for radius_values in radius_values_list:
+    capt_radius, comm_radius = radius_values.split(" ")
     for instance_name in instance_names_list:
 
         with open("run_parameters.dat", "w") as parameters_file:                
             with open("parameters_template.dat", "r") as template_parameters_file:
                 for template_line in template_parameters_file:
-                    parameters_file.write(template_line.replace("INSTANCE_NAME", instance_name).replace("CAPT_RADIUS COMM_RADIUS", radius_values))
+                    parameters_file.write(template_line.replace("INSTANCE_NAME", instance_name).replace("CAPT_RADIUS", capt_radius).replace("COMM_RADIUS", comm_radius))
         os.system("echo " + instance_name + " " + radius_values)
         os.system("Metaheuristics_solution\\Debug\\Metaheuristics_solution.exe")
